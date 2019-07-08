3307f8edfbb262156e5b7e1bfcd2b47d42397abd
wl <xxzhwl@163.com>
2019-07-08 15:58:37 +0800

    The first commit

---
+++ src/com/wl/nuaa/Servlet/AddUser.java
@@ -0,0 +1,28 @@
+package com.wl.nuaa.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName: ${ClassName}
+ * @Author：wl
+ * @Date：2019/7/8
+ * @Description:${PACKAGE_NAME}
+ */
+@WebServlet(name = "AddUser",urlPatterns = "/addUser")
+public class AddUser extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.print("用户名:"+userName+"  密码："+password);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xxzhwl
+  Date: 2019/7/8
+  Time: 14:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>HelloWeb！</title>
+  </head>
+  <body>
+
+  <form action="addUser" method="post">
+    用户名：<input name="username" type="text"><br/>
+    密码：  <input name="password" type="password"><br/>
+    <input type="submit" name="提交">
+  </form>
+  </body>
+</html>
